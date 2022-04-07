76d6766537b50dac2b260a2d1c7b534fa90b8d39
Alexa <ro.alexandra.petrea@gmail.com>
2022-04-07 13:58:41 +0000

    quick pagination hack

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
+import {useState} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Row, Col } from 'react-bootstrap';
 
 import Header from './components/Header';
 import ProductList from './containers/ProductList';
 
+export const PAGE_HOME = 'home';
+export const PAGE_SECOND = 'second';
+export const PAGE_PRODUCT_LIST = 'productlist';
+
 function App() {
+  const [page, setPage] = useState(PAGE_HOME);
+
   return (
     <div className="App">
-      <Header />
+      <Header page={page} setPage={setPage} />
       <Container fluid="md">
-        <Row>
-          <Col><ProductList /></Col>
-        </Row>
+        {page === PAGE_HOME && 
+          <div> HOME PAGE</div>
+        }        
+        {page === PAGE_SECOND && 
+          <div> SECOND PAGE</div>
+        }
+        {page === PAGE_PRODUCT_LIST && 
+          <Row>
+            <Col><ProductList /></Col>
+          </Row>
+        }
       </Container>
     </div>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,16 @@
 import {Navbar, Container, Nav} from 'react-bootstrap';
+import {PAGE_HOME, PAGE_SECOND, PAGE_PRODUCT_LIST} from '../App'
 
-function Header() {
+function Header({page, setPage}) {
     return (
     <Navbar bg="light" expand="lg">
     <Container>
-        <Navbar.Brand href="#home">Alexa's Shop</Navbar.Brand>
+        <Navbar.Brand href="#" onClick={() => setPage(PAGE_HOME)}>Alexa's Shop</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="me-auto">
-            <Nav.Link href="#home">Product list</Nav.Link>
+            <Nav.Link  href="#" onClick={() => setPage(PAGE_SECOND)}>Second Page</Nav.Link>
+            <Nav.Link  href="#" onClick={() => setPage(PAGE_PRODUCT_LIST)}>Product list</Nav.Link>
         </Nav>
         </Navbar.Collapse>
     </Container>
